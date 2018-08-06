2ff022a037ec8ff765d16a40bf357c9d95e7fb70
Jonathan K. Wolski <jonathan.k.wolski@disney.com>
2018-08-06 08:28:36 -0700

    equash! extract method `findPrimaryLanguage`

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ const bcp47 = require('bcp-47')
 const iso639 = require('iso-639-3')
 const { find, keyBy } = require('lodash')
 
+function primaryBy2Char(lang) {
+  return lang.iso6391;
+}
+
+function primaryBy3Char(lang) {
+  // https://tools.ietf.org/html/rfc5646#section-2.2.1
+  return (lang.iso6393 || lang.iso6392T || lang.iso6392B)
+}
+
+function findPrimaryLanguage(code) {
+   return keyBy(iso639, code.length === 2 ? primaryBy2Char : primaryBy3Char)[code]
+}
 
 function describeLang(tag) {
   const parsed = bcp47.parse(tag, {normalize: true})
-
-  return find(iso639, function(lang) {
-    // TODO handle the case where parsed.language is null or undefined
-    if (parsed.language.length === 2 && lang.iso6391 === parsed.language) {
-      return true
-    } else if ((lang.iso6393 || lang.iso6392T || lang.iso639B) === parsed.language) {
-      return true
-    } else {
-      return false
-    }
-  }).name
+  return findPrimaryLanguage(parsed.language).name
 }
 
 module.exports = describeLang
